1f20bb5465a2c31b210131c1b760434acd0be53e
aterletsky123 <terletsky28@gmail.com>
2022-07-08 19:11:01 +0300

    added after banner block, html structure for seo-block

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  readMoreStatus = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleClassActive(element: HTMLElement): void {
+    element.classList.toggle('active');
+    this.readMoreStatus ? this.readMoreStatus = false : this.readMoreStatus = true;
+    // добавити типу якщо клік був не на менюшці закривати його
   }
 
 }
